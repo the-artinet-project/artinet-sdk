3d9a907b18c01b09d351401c4a8cc3663cd77acd
EditUndo <patrick.buah.jr@gmail.com>
2026-01-05 00:48:14 +0000

    Refactor nock setup in test configuration
    - Removed the redundant call to disableNetConnect in the beforeAll hook.
    - Commented out the enableNetConnect function to clarify the intent of allowing local network connections only.

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,7 +1,14 @@
 import nock from "nock";
 
+nock.disableNetConnect();
+// nock.enableNetConnect((host) => {
+//   return (
+//     host.includes("localhost") ||
+//     host.includes("127.0.0.1") ||
+//     host.match(/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/)
+//   ); // Local IPs
+// });
 beforeAll(() => {
-  nock.disableNetConnect();
   nock.enableNetConnect((host) => {
     return host.includes("localhost") || host.includes("127.0.0.1");
   });
