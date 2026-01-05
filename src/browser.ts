43563150a5df2d22927f48bd1356bcc1c8bca7ce
EditUndo <patrick.buah.jr@gmail.com>
2026-01-05 21:34:35 +0000

    Update browser entry point to export from messenger module instead of client module

--- src/browser.ts
+++ src/browser.ts
@@ -1,6 +1,6 @@
 //browser only entry point
 export * from "./types/index.js";
-export * from "./client/index.js";
+export * from "./messenger/index.js";
 
 export * from "./utils/index.js";
 export * from "./config/index.js";
