4c60c9d039bbe5386ba50e6c0cd8f5ec9d25557e
EditUndo <patrick.buah.jr@gmail.com>
2026-01-22 19:21:59 +0000

    feat: Enhance Manager class with storage support and async methods
    - Implemented core.Manager interface in the Manager class to standardize behavior.
    - Added optional storage parameter for persistence, allowing data to be stored and retrieved asynchronously.
    - Updated set, get, delete, has, and list methods to interact with the storage layer when available.
    - Included TODO comments for potential future improvements in storage initialization and data handling.

--- src/services/core/manager.ts
+++ src/services/core/manager.ts
@@ -4,20 +4,21 @@
  */
 
 import { logger } from "~/config/index.js";
+import { core } from "~/types/index.js";
 
-//TODO: Add persistence layer plugin support and turn Manager into an LRU cache.
-/**
- * Manager should optionally take a persistent storage object as a constructor parameter,
- * That way we don't need to re-implement the same caching logic for each derived manager.
- */
-export abstract class Manager<T> {
+export abstract class Manager<T> implements core.Manager<T> {
   constructor(
     private _data: Map<string, T> = new Map(),
-    private throwOnSet: boolean = true
-  ) {}
+    private throwOnSet: boolean = true,
+    private storage?: core.Manager<T>
+  ) {
+    //TODO: consider async initialization of storage/lazy loading of cold data (to an upper-bound)
+  }
+
   get data(): Map<string, T> {
     return this._data;
   }
+  
   async set(id: string, data?: T): Promise<void> {
     logger.debug(`${this.constructor.name}[set]:`, { id });
     if (!data && this.throwOnSet) {
@@ -26,22 +27,48 @@ export abstract class Manager<T> {
       return;
     }
     this.data.set(id, data);
+    if (this.storage) {
+      await this.storage.set(id, data);
+    }
   }
+  
   async get(id: string): Promise<T | undefined> {
-    // logger.debug(`${this.constructor.name}[get]:`, { id });
-    return this.data.get(id);
+    const data = this.data.get(id);
+    if (data) {
+      return data;
+    }
+    if (this.storage) {
+      return await this.storage.get(id);
+    }
+    return undefined;
   }
+  
   async delete(id: string): Promise<void> {
     logger.debug(`${this.constructor.name}[delete]:`, { id });
+    if (this.storage) {
+      /** Probably best to delete from storage first to avoid race conditions. */
+      await this.storage.delete(id);
+    }
     this.data.delete(id);
   }
+  
   async has(id: string): Promise<boolean> {
-    // logger.debug(`${this.constructor.name}[has]:`, { id });
-    return this.data.has(id);
+    if (this.data.has(id)) {
+      return true;
+    }
+    if (this.storage) {
+      return await this.storage.has?.(id) ?? false;
+    }
+    return false;
   }
+  
   async list(): Promise<T[]> {
-    // logger.debug(`${this.constructor.name}[list]:`);
-    return Array.from(this.data.values());
+    const listed = Array.from(this.data.values());
+    if (this.storage) {
+      /** Could be an expensive operation */
+      return await this.storage.list?.() ?? [];
+    }
+    return listed;
   }
 }
 export const ResourceManager = Manager;
