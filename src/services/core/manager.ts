91bd884898646b8aad1d3d55a0ac633ef51e82cd
EditUndo <patrick.buah.jr@gmail.com>
2026-01-24 13:36:15 +0000

    refactor: Update Manager class to use cache instead of data property
    - Renamed the internal data storage from `_data` to `_cache` for clarity.
    - Updated methods to interact with the new cache property, including set, get, delete, has, and list.
    - Deprecated the old data property in favor of the new cache implementation, ensuring backward compatibility.

--- src/services/core/manager.ts
+++ src/services/core/manager.ts
@@ -3,72 +3,81 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { logger } from "~/config/index.js";
-import { core } from "~/types/index.js";
+import { logger } from '~/config/index.js';
+import { core } from '~/types/index.js';
 
 export abstract class Manager<T> implements core.Manager<T> {
-  constructor(
-    private _data: Map<string, T> = new Map(),
-    private throwOnSet: boolean = true,
-    private storage?: core.Manager<T>
-  ) {
-    //TODO: consider async initialization of storage/lazy loading of cold data (to an upper-bound)
-  }
+    constructor(
+        private _cache: Map<string, T> = new Map(),
+        private throwOnSet: boolean = true,
+        private storage?: core.Manager<T>,
+    ) {
+        //TODO: consider async initialization of storage/lazy loading of cold data (to an upper-bound)
+    }
 
-  get data(): Map<string, T> {
-    return this._data;
-  }
-  
-  async set(id: string, data?: T): Promise<void> {
-    logger.debug(`${this.constructor.name}[set]:`, { id });
-    if (!data && this.throwOnSet) {
-      throw new Error("Data is required");
-    } else if (!data) {
-      return;
+    get cache(): Map<string, T> {
+        return this._cache;
     }
-    this.data.set(id, data);
-    if (this.storage) {
-      await this.storage.set(id, data);
+
+    /** @deprecated use cache instead */
+    get data(): Map<string, T> {
+        return this.cache;
     }
-  }
-  
-  async get(id: string): Promise<T | undefined> {
-    const data = this.data.get(id);
-    if (data) {
-      return data;
+
+    async set(id: string, data?: T): Promise<void> {
+        logger.debug(`${this.constructor.name}[set]:`, { id });
+        if (!data && this.throwOnSet) {
+            throw new Error('Data is required');
+        } else if (!data) {
+            return;
+        }
+        this.cache.set(id, data);
+        if (this.storage) {
+            await this.storage.set(id, data);
+        }
     }
-    if (this.storage) {
-      return await this.storage.get(id);
+
+    async get(id: string): Promise<T | undefined> {
+        let data: T | undefined = this.cache.get(id);
+        if (!data && this.storage) {
+            data = await this.storage.get(id);
+            if (data) {
+                this.cache.set(id, data);
+            }
+        }
+        return data;
     }
-    return undefined;
-  }
-  
-  async delete(id: string): Promise<void> {
-    logger.debug(`${this.constructor.name}[delete]:`, { id });
-    if (this.storage) {
-      /** Probably best to delete from storage first to avoid race conditions. */
-      await this.storage.delete(id);
+
+    async delete(id: string): Promise<void> {
+        logger.debug(`${this.constructor.name}[delete]:`, { id });
+        if (this.storage) {
+            /** Probably best to delete from storage first to avoid race conditions. */
+            await this.storage.delete(id);
+        }
+        this.cache.delete(id);
     }
-    this.data.delete(id);
-  }
-  
-  async has(id: string): Promise<boolean> {
-    if (this.data.has(id)) {
-      return true;
+
+    async has(id: string): Promise<boolean> {
+        if (this.cache.has(id)) {
+            return true;
+        }
+        if (this.storage) {
+            const data: T | undefined = await this.storage.get(id);
+            if (data) {
+                this.cache.set(id, data);
+                return true;
+            }
+        }
+        return false;
     }
-    if (this.storage) {
-      return await this.storage.has?.(id) ?? false;
+
+    async list(): Promise<T[]> {
+        const listed = Array.from(this.cache.values());
+        if (this.storage) {
+            /** Could be an expensive operation */
+            return (await this.storage.list?.()) ?? [];
+        }
+        return listed;
     }
-    return false;
-  }
-  
-  async list(): Promise<T[]> {
-    const listed = Array.from(this.data.values());
-    if (this.storage) {
-      /** Could be an expensive operation */
-      return await this.storage.list?.() ?? [];
-    }
-    return listed;
-  }
 }
 export const ResourceManager = Manager;
